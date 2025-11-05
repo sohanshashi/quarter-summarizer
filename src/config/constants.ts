2c84b94587e8493fc6db542e67d1840e2a5ea4d9
Sohan Shashikumar <sohan.shashikumar@hackerrank.com>
2025-11-06 01:03:10 +0530

    add url constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,3 +2,7 @@ export const GITHUB_CONSTANTS = Object.freeze({
   API_BASE_URL: 'https://api.github.com',
   API_REQUEST_TIMEOUT: 10000,
 });
+
+export const API_URLS = Object.freeze({
+  getPullRequests: () => 'TODO',
+});
