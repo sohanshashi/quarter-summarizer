9236c5c75cf01beb30011e8738b9285f3e607645
Sohan Shashikumar <sohan.shashikumar@hackerrank.com>
2025-11-22 20:01:08 +0530

    use server sent events for summary controller root route

--- src/summary/summary.controller.ts
+++ src/summary/summary.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Query, Sse } from '@nestjs/common';
 
 import { SummaryService } from './summary.service';
 import { GetSummaryQueryDto } from './dto/get_summary_query_dto';
@@ -7,8 +7,8 @@ import { GetSummaryQueryDto } from './dto/get_summary_query_dto';
 export class SummaryController {
   constructor(private readonly summaryService: SummaryService) {}
 
-  @Get()
-  async getSummary(@Query() query: GetSummaryQueryDto) {
+  @Sse()
+  async *getSummary(@Query() query: GetSummaryQueryDto) {
     const { username, orgName, startDate, endDate, model } = query;
 
     const pullRequests = await this.summaryService.getPullRequests({
@@ -22,10 +22,12 @@ export class SummaryController {
 
     for await (const chunk of stream) {
       if (chunk.choices[0].delta.content) {
-        process.stdout.write(chunk.choices[0].delta.content);
+        yield {
+          data: {
+            content: chunk.choices[0].delta.content,
+          },
+        } as MessageEvent;
       }
     }
-
-    return pullRequests;
   }
 }
