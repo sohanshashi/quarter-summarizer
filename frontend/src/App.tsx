666b737faad7a2d08c0c5b3d30b848b2d80703d4
Sohan Shashikumar <sohan.shashikumar@hackerrank.com>
2025-11-23 14:44:01 +0530

    add theme provider to switch between light and dark themes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-background text-foreground">
-      <p>Hello world</p>
       <Button>Click me</Button>
     </div>
   );
