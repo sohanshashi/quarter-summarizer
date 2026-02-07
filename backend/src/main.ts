e879e7243d23e45f6807646f16efcdb2574608d0
Sohan Shashikumar <sohan.shashikumar@hackerrank.com>
2026-02-07 09:13:22 +0530

    add promise rejection handler to bootstrap method

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,4 +17,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) =>
+  console.error('Unable to bootstrap nest application', err),
+);
