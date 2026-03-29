77f6b1d2ac7cc9879ef9df2411d12f933b1df9ea
Sohan Shashikumar <sohan.shashikumar@hackerrank.com>
2026-03-29 13:14:06 +0530

    modify comment in Dockerfile and fix incorrect template path for summarization

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -4,7 +4,7 @@ export const LLM_CONSTANTS = Object.freeze({
   CLASSIFICATION_SYSTEM_TEMPLATE: 'system/classify_prs.njk',
   CLASSIFICATION_USER_TEMPLATE: 'user/classify_prs.njk',
   SUMMARIZATION_SYSTEM_TEMPLATE: 'system/summarize_prs.njk',
-  SUMMARIZATION_USER_TEMPLATE: 'system/summarize_prs.njk',
+  SUMMARIZATION_USER_TEMPLATE: 'user/summarize_prs.njk',
   OLLAMA_BASE_URL: 'http://host.docker.internal:11434/v1',
   OLLAMA_API_KEY: 'ollama',
 });
